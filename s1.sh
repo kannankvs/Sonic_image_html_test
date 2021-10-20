626d6a65f604553b70ef21e18dcd3ecb22a64a21
kannankvs <kannan_kvs@dell.com>
2021-10-20 14:09:03 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -3,8 +3,7 @@ echo '{ KVSK1' > s1.json
 echo "\n}" >> s1.json
 git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
+git push origin BR1
 git add s1.json
 git commit -m "d2 latest links for sonic images"
-git fetch
-git rebase origin/BR1 BR1
-git push origin BR1:refs/for/BR1
+git push --set-upstream origin BR1
