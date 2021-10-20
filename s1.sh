26a2db31366e61656d5c969696849d1f439294e0
kannankvs <kannan_kvs@dell.com>
2021-10-20 20:22:09 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -2,9 +2,8 @@
 git checkout -b BR1
 git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
-git pull origin BR1
-git push origin BR1
-echo '{ KVSK- is it working now in test1?' > s1.json
+git pull --ff-only origin BR1
+echo '{ KVSK- is it working now in test2: 8:22pm?' > s1.json
 echo "\n}" >> s1.json
 git add s1.json
 git commit -m "latest links for sonic images"
